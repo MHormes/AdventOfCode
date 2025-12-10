8e59659d2a1d73667f69328dc5aba6f604be3c20
Maarten Hormes <maarten.hormes@gmail.com>
2025-12-10 15:35:04 +0100

    failed attempt at D2P2

--- 2025/02/02_solution_2.js
+++ 2025/02/02_solution_2.js
@@ -16,23 +16,40 @@ for (let range of input) {
     while (currentChecking <= end) {
         let currentCheckingString = currentChecking.toString();
         const zeroCheckDigit = currentCheckingString.split("")
-    
+
         if (zeroCheckDigit[0] === "0") {
             log("Leading zero found in number: ", currentChecking);
             invalidCount += currentChecking
         }
 
         let hasDouble = false;
-        
-        const length = currentCheckingString.length;
-            let part1 = currentCheckingString.substring(0, length/2);
-            let part2 = currentCheckingString.substring(length/2, length);
 
-            if(part1 === part2){   
+        const length = currentCheckingString.length;
+
+        for (let index = 0; index < length / 2; index++) {
+            let part1 = currentCheckingString.substring(0, length / 2 - index);
+            let part2 = currentCheckingString.substring(length / 2 - index, length - (index * 2));
+            if (part1 === part2 && index === 0) {
                 hasDouble = true;
+            } else if (part1 === part2) {
+                const amountOfParts = 2 + index;
+                let parts = [];
+                let splitIndex = 0;
+                for (let checkIndex = 0; checkIndex < amountOfParts; checkIndex++) {
+                    let part = currentCheckingString.substring(splitIndex, splitIndex + part1.length);
+                    splitIndex += part1.length;
+                    parts.push(part);
+                }
+                const allEqual = parts.every(p => p === part1);
+                if (allEqual) {
+                    hasDouble = true;
+                }
             }
-        
-        if(hasDouble == true){
+        }
+
+
+
+        if (hasDouble == true) {
             invalidCount += currentChecking;
         }
 
@@ -40,4 +57,6 @@ for (let range of input) {
     }
 }
 
+
+// SOLUTION IS INCCORECT! DID NOT MANAGE TO FIND THE RIGHT PATTERN
 log("Invalid Count: ", invalidCount);
